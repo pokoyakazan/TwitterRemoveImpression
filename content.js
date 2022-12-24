f8d5403c540642584cdb37167635c7233ddc1994
pokoyakazan <okuyama1625@gmail.com>
2022-12-24 23:54:16 +0900

    要素ごと消してしまう

--- content.js
+++ content.js
@@ -4,9 +4,9 @@ function rm_imp(){
   var activities = document.getElementsByClassName('css-1dbjc4n r-1ta3fxp r-18u37iz r-1wtj0ep r-1s2bzr4 r-1mdbhws');
   for (let i=0;i<activities.length;i++){
     var imp = activities[i]['childNodes'][0]
-    if (imp.firstElementChild.firstElementChild.childElementCount == 2){
-      var imp_num = imp.firstElementChild.firstElementChild.lastElementChild.firstElementChild.firstElementChild.firstElementChild
-      imp_num.textContent = ''
+
+    while (imp.firstChild) {
+      imp.removeChild(imp.firstChild);
     }
   }
 }
